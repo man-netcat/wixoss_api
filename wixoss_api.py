0a4aa0af320e07ec4469452a8d4e0d5587e62e8a
Rick Teuthof <rmt22198@outlook.com>
2022-02-24 16:50:20 +0100

    Recombined functions

--- wixoss_api.py
+++ wixoss_api.py
@@ -1,6 +1,3 @@
-import csv
-import json
-import os
 import shutil
 from os import path
 import sqlite3
@@ -14,7 +11,7 @@ query = '/Carddb?nomecarta=&cerca=true&ordina=cardid&page=%s'
 session = CachedSession()
 
 
-def make_json():
+def main():
     database = []
 
     i = 1
@@ -38,6 +35,13 @@ def make_json():
             # Get card id
             img_href = container.find('img')['src']
             cardid = img_href.split('/')[2]
+            img_path = f'images/{cardid}.png'
+
+            # Download img
+            if not path.exists(img_path):
+                img_res = session.get(base_url + img_href, stream=True)
+                with open(img_path, 'wb') as out_file:
+                    shutil.copyfileobj(img_res.raw, out_file)
 
             # Write cardinfo to db
             cardinfo = container.find(
@@ -56,45 +60,6 @@ def make_json():
     df = pd.DataFrame(database)
     conn = sqlite3.connect('database.db')
     df.to_sql("cards", conn, if_exists='append', index=False)
-
-
-def download_images():
-    i = 1
-    while True:
-        req = session.get(base_url + query % i)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        cards = soup.find('div', class_='risultati')
-
-        # Reached end of pages
-        if not cards:
-            break
-
-        for card in cards.find_all('a', class_='preview'):
-            href = card['href']
-            cardreq = session.get(base_url + href)
-            cardsoup = BeautifulSoup(cardreq.content, 'html.parser')
-
-            # Get main data container
-            container = cardsoup.find('main', id='main-content')
-
-            # Get image properties
-            img_href = container.find('img')['src']
-            cardid = img_href.split('/')[2]
-            img_path = f'images/{cardid}.png'
-
-            # Download img
-            if not path.exists(img_path):
-                img_res = session.get(base_url + img_href, stream=True)
-                with open(img_path, 'wb') as out_file:
-                    shutil.copyfileobj(img_res.raw, out_file)
-        i += 1
-
-    session.close()
-
-
-def main():
-    make_json()
-    download_images()
 
 
 if __name__ == '__main__':
