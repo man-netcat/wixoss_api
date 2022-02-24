5aba1b0e2c5f354e3f06fdfe7bcec605facbf0e3
Rick Teuthof <rmt22198@outlook.com>
2022-02-24 01:24:34 +0100

    Separate database and image downloading

--- wixoss_api.py
+++ wixoss_api.py
@@ -1,59 +1,106 @@
 import json
+import os
 import shutil
-import sqlite3
 from os import path
+import types
 
 from bs4 import BeautifulSoup
 from requests_cache import CachedSession
 
-conn = sqlite3.connect('database.db')
 
-session = CachedSession()
+def make_json():
+    session = CachedSession()
+    base_url = 'https://www.wixosstcg.eu'
+    query = '/Carddb?nomecarta=&cerca=true&ordina=cardid&page=%s'
 
-base_url = "https://www.wixosstcg.eu"
-query = "/Carddb?nomecarta=&cerca=true&ordina=cardid&page=%s"
+    database = []
 
-database = {}
+    i = 1
+    while True:
+        req = session.get(base_url + query % i)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        cards = soup.find('div', class_='risultati')
 
-# Hardcoded range because lazy
-i = 1
-while True:
-    req = session.get(base_url + query % i)
-    soup = BeautifulSoup(req.content, 'html.parser')
-    cards = soup.find('div', class_='risultati')
+        # Reached end of pages
+        if not cards:
+            break
 
-    # Reached end of pages
-    if not cards:
-        break
+        for card in cards.find_all('a', class_='preview'):
+            href = card['href']
+            cardreq = session.get(base_url + href)
+            cardsoup = BeautifulSoup(cardreq.content, 'html.parser')
 
-    for card in cards.find_all('a', class_='preview'):
-        href = card['href']
-        cardreq = session.get(base_url + href)
-        cardsoup = BeautifulSoup(cardreq.content, 'html.parser')
+            # Get main data container
+            container = cardsoup.find('main', id='main-content')
 
-        # Get main data container
-        container = cardsoup.find('main', id='main-content')
+            # Get card id
+            img_href = container.find('img')['src']
+            cardid = img_href.split('/')[2]
 
-        # Get image properties
-        img_href = container.find('img')['src']
-        cardid = img_href.split('/')[2]
-        img_path = f'images/{cardid}.png'
+            # Write cardinfo to db
+            cardinfo = container.find(
+                'div', class_='col-md-6 padding-top-30').find_all('div')
+            cardentry = {'Card Id': cardid}
+            for key, value in zip(cardinfo[0::2], cardinfo[1::2]):
+                stripped_key = key.get_text().strip()
+                stripped_value = value.get_text().strip()
+                cardentry[stripped_key] = stripped_value
+            database.append(cardentry)
+        i += 1
 
-        # Download img
-        if not path.exists(img_path):
-            img_res = session.get(base_url + img_href, stream=True)
-            with open(img_path, 'wb') as out_file:
-                shutil.copyfileobj(img_res.raw, out_file)
+    session.close()
 
-        # Write cardinfo to db
-        cardinfo = container.find(
-            'div', class_='col-md-6 padding-top-30').find_all('div')
-        database[cardid] = {}
-        for key, value in zip(cardinfo[0::2], cardinfo[1::2]):
-            stripped_key = key.get_text().strip()
-            stripped_value = value.get_text().strip()
-            database[cardid][stripped_key] = stripped_value
-    i += 1
+    # Write dict to json
+    with open('database.json', 'w', encoding='utf8') as f:
+        json.dump(database, f, ensure_ascii=False)
 
-with open('database.json', 'w', encoding='utf8') as outfile:
-    json.dump(database, outfile, ensure_ascii=False)
+
+def download_images():
+    session = CachedSession()
+    base_url = 'https://www.wixosstcg.eu'
+    query = '/Carddb?nomecarta=&cerca=true&ordina=cardid&page=%s'
+
+    i = 1
+    while True:
+        req = session.get(base_url + query % i)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        cards = soup.find('div', class_='risultati')
+
+        # Reached end of pages
+        if not cards:
+            break
+
+        for card in cards.find_all('a', class_='preview'):
+            href = card['href']
+            cardreq = session.get(base_url + href)
+            cardsoup = BeautifulSoup(cardreq.content, 'html.parser')
+
+            # Get main data container
+            container = cardsoup.find('main', id='main-content')
+
+            # Get image properties
+            img_href = container.find('img')['src']
+            cardid = img_href.split('/')[2]
+            img_path = f'images/{cardid}.png'
+
+            # Download img
+            if not path.exists(img_path):
+                img_res = session.get(base_url + img_href, stream=True)
+                with open(img_path, 'wb') as out_file:
+                    shutil.copyfileobj(img_res.raw, out_file)
+        i += 1
+
+    session.close()
+
+
+def main():
+    if not path.exists('database.json'):
+        make_json()
+
+    if not path.exists('images'):
+        os.mkdir('images')
+        download_images()
+
+
+if __name__ == '__main__':
+    main()
